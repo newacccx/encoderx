ebead92ca314eb22f72942f1fafdf07388e37233
newacccx <119074899+newacccx@users.noreply.github.com>
2024-01-23 23:56:55 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -91,7 +91,7 @@ async def incoming_start_message_f(bot, update):
                 ]
             ]
         ),
-        reply_to_message_id=update.message_id,
+        reply_to_message_id=update.id,
     )
     
 async def incoming_compress_message_f(update):
@@ -191,7 +191,7 @@ async def incoming_compress_message_f(update):
   sent_message = await bot.send_message(
   chat_id=update.chat.id,
   text=Localisation.DOWNLOAD_START,
-  reply_to_message_id=update.message_id
+  reply_to_message_id=update.id
               )
   chat_id = LOG_CHANNEL
   utc_now = datetime.datetime.utcnow()
@@ -350,7 +350,7 @@ async def incoming_compress_message_f(update):
         force_document=True,
         #duration=duration,
         thumb="thumb.jpg",
-        reply_to_message_id=update.message_id,
+        reply_to_message_id=update.id,
         progress=progress_for_pyrogram,
         progress_args=(
           bot,
@@ -452,5 +452,5 @@ async def incoming_cancel_message_f(bot, update):
     await bot.send_message(
       chat_id=update.chat.id,
       text="No active compression exists",
-      reply_to_message_id=update.message_id
+      reply_to_message_id=update.id
     )
